5aa5741cb56e122bc9e833f712bc93461fdf5f11
Krishna Kotni <testmailprojectpython@gmail.com>
2022-11-21 12:36:24 +0530

    login update

--- app.py
+++ app.py
@@ -1,45 +1,72 @@
-from flask import Flask, request as req, jsonify, render_template, redirect
+from django.http import response
+from flask import Flask, request as req, jsonify, render_template, redirect, make_response
+import requests
 
 app = Flask(__name__)
 
-@app.route("/login")
+backend_url = "http://localhost:5000"
+
+@app.route("/login", methods=["GET","POST"])
 def login():
-    return render_template("login.html")
+    if req.method == "GET":
+        return render_template("login.html")
+    else:
+        r = dict(req.form)
+        email = r['email']
+        password = r['password']
+        print("Email: {}".format(email))
+        print("Password: {}".format(password))
+        
+        r = requests.post(backend_url + "/login",json = {"email": email, "password": password})
+        
+        print(r.json())
+        
+        res = make_response(redirect("/"))
+        res.set_cookie("auth", email)
+        
+        if r.status_code == 400:
+            return render_template("login.html")
+        else:
+            return res
 
-@app.route("/login/data", methods=['POST'])
-def login_data():
-    r = req.get_json()
-    username = r['user']
-    password = r['pass']
-    print("Username: {}".format(username))
-    print("Password: {}".format(password))
-    return r
-
-@app.route("/signup")
+@app.route("/signup", methods = ["GET", "POST"])
 def signup():
-    return render_template("signup.html")
+    if req.method == "GET":
+        return render_template("signup.html")
+    else:    
+        r = dict(req.form)
 
-@app.route("/signup/data", methods=['POST'])
-def signup_data():
-    r = req.get_json()
-    username = r['user']
-    email = r['email']
-    password = r['pass']
-    cpassword = r['cpass']
-    print("Username: {}".format(username))
-    print("Email: {}".format(email))
-    print("Password: {}".format(password))
-    print("CPassword: {}".format(cpassword))
-    return r
+        email = r['email']
+        password = r['password']
+        cpassword = r['cpassword']
+        
+        print("Email: {}".format(email))
+        print("Password: {}".format(password))
+        print("CPassword: {}".format(cpassword))
+        
+        r = requests.post(backend_url + "/signup",json = {"email": email, "password": password})
+        
+        print(r.json())
+        
+        res = make_response(redirect("/"))
+        res.set_cookie("auth", email)
+        
+        if r.status_code == 400:
+            return render_template("signup.html")
+        else:
+            return res
 
-@app.route("/index")
-def index():
-    return render_template("index.html")
+
+@app.route("/logout", methods = ["GET"])
+def logout():
+        res = make_response(redirect("/login"))
+        res.delete_cookie("auth")
+        return res
 
 @app.route("/", methods = ["GET"])
 def home():
     ## check cookie
-    if req.cookies.get("auth"):
+    if req.cookies.get("auth") != None:
         print("user has cookies")
         return render_template("index.html")
     else:
@@ -47,4 +74,4 @@ def home():
         return redirect("/login")
     
 if __name__ == "__main__":
-    app.run(port=5000, debug=True)
+    app.run(port=3000, debug=True)
